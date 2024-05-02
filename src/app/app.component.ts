a57e477b8430b83c327ca6a62885e8e670f0d644
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2024-05-02 15:42:09 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
-import { AppFooterComponent } from './components/app-footer/app-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppHeaderComponent, AppFooterComponent,],
+  imports: [RouterOutlet, AppHeaderComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
