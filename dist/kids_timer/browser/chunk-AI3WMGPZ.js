001159cd4a82c661bd7140554065f379493f64b4
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2024-05-02 15:40:44 +0200

    update

--- dist/kids_timer/browser/chunk-AI3WMGPZ.js
+++
@@ -1,1 +0,0 @@
-import{L as o}from"./chunk-A3B6N2TJ.js";var e=(()=>{let t=class t{constructor(){this._isSoundOn=!0}get isSoundOn(){return this._isSoundOn}toggleSound(){this._isSoundOn=!this._isSoundOn}};t.\u0275fac=function(i){return new(i||t)},t.\u0275prov=o({token:t,factory:t.\u0275fac,providedIn:"root"});let n=t;return n})();export{e as a};
